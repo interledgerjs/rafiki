d71b7a07ce77be0cb47d0e74ac96f899a7fb81dc
DonChangfoot <donovan@sportraxs.com>
2019-03-05 09:16:56 +0200

    fix (settlement engine): start polling redis stream on startup

--- src/services/settlement-engine.ts
+++ src/services/settlement-engine.ts
@@ -17,20 +17,35 @@ export default class SettlementEngine {
 
   redis: Redis.Redis
   streamKey: string
+  lastMessageId: string | undefined
   peerBalances: Map<string, bigint> = new Map()
   peerLimits: Map<string, { min: bigint, max: bigint }> = new Map()
+  polling: NodeJS.Immediate
   private started: boolean = false
   constructor ({ redisClient, streamKey }: SettlementEngineServices) {
     this.redis = redisClient
     this.streamKey = streamKey
+    this.lastMessageId = undefined
   }
 
   async start (): Promise<void> {
     this.started = true
     // TODO: Handle case where redis status isn't connected
+
+    this.startPolling()
+  }
+
+  private startPolling = async (): Promise<void> => {
+    return new Promise(async (resolve) => {
+      await this.process(this.streamKey)
+      this.polling = setImmediate(() => this.startPolling())
+      this.polling.unref()
+      resolve()
+    })
   }
 
   async shutdown (): Promise<string> {
+    clearImmediate(this.polling)
     return this.redis.quit()
   }
 
@@ -56,25 +71,29 @@ export default class SettlementEngine {
     return prevbalance
   }
 
-  async updateBalance (peerId: string, delta: bigint) {
+  updateBalance (peerId: string, delta: bigint) {
     const prevbalance = this.getBalance(peerId)
     const newBalance = prevbalance + delta
     const limits = this.getBalanceLimits(peerId)
     const isNewBalanceOutsideLimits = newBalance < limits.min || newBalance > limits.max
     const prevBalanceOutsideLimits = prevbalance < limits.min || prevbalance > limits.max
 
-    if (isNewBalanceOutsideLimits) await this.redis.set(`${peerId}:balance:enabled`, false)
-    if (prevBalanceOutsideLimits && !isNewBalanceOutsideLimits) await this.redis.set(`${peerId}:balance:enabled`, true)
+    if (isNewBalanceOutsideLimits) this.redis.set(`${peerId}:balance:enabled`, false)
+    if (prevBalanceOutsideLimits && !isNewBalanceOutsideLimits) this.redis.set(`${peerId}:balance:enabled`, true)
 
     this.peerBalances.set(peerId, newBalance)
   }
 
-  async process (streamKey: string): Promise<void> {
-    const data = await this.redis.xrange(streamKey, '-', '+', 'COUNT', '1')
-    const message = this.parseMessageIntoObject(data[0][1])
+  process = async (streamKey: string): Promise<void> => {
+    const redisQuery = this.lastMessageId ? this.lastMessageId : '$'
+    const data = await this.redis.xread('COUNT', 1, 'STREAMS', streamKey, redisQuery)
 
-    if (message.pipeline === 'incoming') this.handleIncomingPipelineMessage(message)
-    else this.handleOutgoingPipelineMessage(message)
+    if (data[0][1].length > 0) {
+      this.lastMessageId = data[0][1][0][0]
+      const message = this.parseMessageIntoObject(data[0][1][0][1])
+      if (message.pipeline === 'incoming') this.handleIncomingPipelineMessage(message)
+      else this.handleOutgoingPipelineMessage(message)
+    }
   }
 
   parseMessageIntoObject (data: Array<string>): BalanceMiddlewareMessage {
