a8e00a806c36c6698b3f2cd026abd21cc05f5451
Matt de Haast <matt.dehaast@coil.com>
2019-03-06 14:53:06 +0200

    fix: removePeer and shutdown on app
    * Ensure it cleans up the maps and endpoints properly by deleting them

--- src/app.ts
+++ src/app.ts
@@ -104,6 +104,10 @@ export default class App {
     if (endpoint) {
       endpoint.close()
     }
+    this.packetCacheMap.delete(peerId)
+    this.rateLimitBucketMap.delete(peerId)
+    this.throughputBucketsMap.delete(peerId)
+    this.endpointsMap.delete(peerId)
     await this.connector.removePeer(peerId)
     // TODO need to resolve some stuff here
   }
@@ -112,7 +116,7 @@ export default class App {
    * Tells connector to remove its peers and clears the stored packet caches and token buckets. The connector is responsible for shutting down the peer's middleware.
    */
   async shutdown () {
-    this.connector.getPeerList().forEach((peerId: string) => this.connector.removePeer(peerId))
+    this.connector.getPeerList().forEach((peerId: string) => this.removePeer(peerId))
     Array.from(this.packetCacheMap.values()).forEach(cache => cache.dispose())
     this.packetCacheMap.clear()
     this.rateLimitBucketMap.clear()
