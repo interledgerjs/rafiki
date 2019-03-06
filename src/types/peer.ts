682720f02a78073e1df02e4819382982c15db8de
Matt de Haast <matt.dehaast@coil.com>
2019-03-06 12:15:29 +0200

    feat(rules): change middleware to rules and protocols

--- src/types/peer.ts
+++ src/types/peer.ts
@@ -1,33 +1,51 @@
 export type Relation = 'parent' | 'child' | 'peer' | 'local'
 
+// export interface PeerInfo {
+//   relation: 'parent' | 'peer' | 'child',
+//   id: string,
+//   assetCode: string,
+//   assetScale: number,
+//   balance?: {
+//     minimum: bigint,
+//     maximum: bigint,
+//     settleThreshold?: bigint,
+//     settleTo: bigint
+//   },
+//   deduplicate?: {
+//     cleanupInterval?: number,
+//     packetLifetime?: number
+//   },
+//   maxPacketAmount?: bigint,
+//   throughput?: {
+//     refillPeriod?: number,
+//     incomingAmount?: bigint,
+//     outgoingAmount?: bigint
+//   },
+//   rateLimit?: {
+//     refillPeriod?: number,
+//     refillCount?: bigint,
+//     capacity?: bigint
+//   },
+//   options?: object,
+//   sendRoutes?: boolean,
+//   receiveRoutes?: boolean,
+//   ilpAddressSegment?: string
+// }
+
 export interface PeerInfo {
   relation: 'parent' | 'peer' | 'child',
   id: string,
   assetCode: string,
   assetScale: number,
-  balance?: {
-    minimum: bigint,
-    maximum: bigint,
-    settleThreshold?: bigint,
-    settleTo: bigint
-  },
-  deduplicate?: {
-    cleanupInterval?: number,
-    packetLifetime?: number
-  },
-  maxPacketAmount?: bigint,
-  throughput?: {
-    refillPeriod?: number,
-    incomingAmount?: bigint,
-    outgoingAmount?: bigint
-  },
-  rateLimit?: {
-    refillPeriod?: number,
-    refillCount?: bigint,
-    capacity?: bigint
-  },
-  options?: object,
-  sendRoutes?: boolean,
-  receiveRoutes?: boolean,
-  ilpAddressSegment?: string
+  rules: Rule[],
+  protocols: Protocol[]
+}
+export interface Rule {
+  name: string,
+  [k: string]: any
+}
+
+export interface Protocol {
+  name: string,
+  [k: string]: any
 }
