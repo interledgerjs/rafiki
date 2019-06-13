ae62e8ab89b5084e88c8a7f2c3b9618deb75dcb9
DonChangfoot <donovan@sportraxs.com>
2019-06-13 16:09:49 +0200

    refactor: admin-api to use koa and joi router

--- src/services/admin-api.ts
+++ src/services/admin-api.ts
@@ -1,11 +1,9 @@
 import { log } from './../winston'
-import { Server, IncomingMessage, ServerResponse } from 'http'
-import Ajv from 'ajv'
+import Koa, { Context } from 'koa'
+import createRouter, { Joi } from 'koa-joi-router'
+import bodyParser from 'koa-bodyparser'
+import { Server, createServer } from 'http'
 import { App } from '../app'
-import * as balanceUpdateSchema from '../schemas/BalanceUpdate.json'
-
-const ajv = new Ajv()
-const validateBalanceUpdate = ajv.compile(balanceUpdateSchema)
 const logger = log.child({ component: 'admin-api' })
 
 export interface AdminApiOptions {
@@ -17,32 +15,15 @@ export interface AdminApiServices {
   app: App
 }
 
-interface Route {
-  method: 'GET' | 'POST' | 'DELETE'
-  match: string
-  fn: (url: string, body: object) => Promise<object | string | void>
-  responseType?: string
-}
-
 export class AdminApi {
+  private _koa: Koa
   private app: App
   private server?: Server
-  private routes: Route[]
   private host: string
   private port: number
 
   constructor ({ host, port }: AdminApiOptions, { app }: AdminApiServices) {
     this.app = app
-    this.routes = [
-      { method: 'GET', match: '/health$', fn: this.getHealth.bind(this) },
-      { method: 'GET', match: '/stats$', fn: this.getStats },
-      { method: 'GET', match: '/alerts$', fn: this.getAlerts },
-      { method: 'GET', match: '/balance$', fn: this.getBalances },
-      { method: 'POST', match: '/peer$', fn: this.addPeer },
-      { method: 'GET', match: '/peer$', fn: this.getPeer },
-      { method: 'GET', match: '/routes$', fn: this.getRoutes }
-    ]
-
     if (host) this.host = host
     if (port) this.port = port
   }
@@ -55,95 +36,65 @@ export class AdminApi {
     const adminApiHost = this.host || '0.0.0.0'
     const adminApiPort = this.port || 7780
 
+    this._koa = this._createKoaApp()
+    this.server = createServer(this._koa.callback()).listen(adminApiPort, adminApiHost)
     logger.info(`admin api listening. host=${adminApiHost} port=${adminApiPort}`)
-    this.server = new Server()
-    this.server.listen(adminApiPort, adminApiHost)
-    this.server.on('request', (req, res) => {
-      logger.debug('Received request: ' + req.url)
-      this.handleRequest(req, res).catch((e) => {
-        let err = e
-        if (!e || typeof e !== 'object') {
-          err = new Error('non-object thrown. error=' + e)
-        }
-
-        // TODO: add logging
-        // log.warn('error in admin api request handler. error=%s', err.stack ? err.stack : err)
-        res.statusCode = e.httpErrorCode || 500
-        res.setHeader('Content-Type', 'text/plain')
-        res.end(String(err))
-      })
-    })
   }
 
-  private async handleRequest (req: IncomingMessage, res: ServerResponse) {
-    req.setEncoding('utf8')
-    let body = ''
-    await new Promise((resolve, reject) => {
-      req.on('data', data => body += data)
-      req.once('end', resolve)
-      req.once('error', reject)
+  private _createKoaApp (): Koa {
+    const koa = new Koa()
+    const router = createRouter()
+    router.use(bodyParser())
+    router.route({
+      method: 'get',
+      path: '/health',
+      handler: async (ctx: Context) => ctx.body = 'Status: ok'
     })
-
-    const urlPrefix = (req.url || '').split('?')[0] + '$'
-    logger.debug('url prefix: ' + urlPrefix)
-    const route = this.routes.find((route) =>
-      route.method === req.method && urlPrefix.startsWith(route.match))
-    if (!route) {
-      res.statusCode = 404
-      res.setHeader('Content-Type', 'text/plain')
-      res.end('Not Found')
-      return
-    }
-
-    const resBody = await route.fn.call(this, req.url, body && JSON.parse(body))
-    if (resBody) {
-      res.statusCode = 200
-      if (route.responseType) {
-        res.setHeader('Content-Type', route.responseType)
-        res.end(resBody)
-      } else {
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify(resBody))
+    router.route({
+      method: 'get',
+      path: '/stats',
+      handler: async (ctx: Context) => ctx.body = this.app.stats.getStatus()
+    })
+    router.route({
+      method: 'get',
+      path: '/alerts',
+      handler: async (ctx: Context) => ctx.body = { alerts: this.app.alerts.getAlerts() }
+    })
+    router.route({
+      method: 'get',
+      path: '/balance',
+      handler: async (ctx: Context) => ctx.body = this.app.getBalances()
+    })
+    router.route({
+      method: 'post',
+      path: '/peer',
+      validate: {
+        body: {
+          peerInfo: Joi.object().required(),
+          endpointInfo: Joi.object().required()
+        },
+        type: 'json'
+      },
+      handler: async (ctx: Context) => {
+        const peerInfo = ctx.request.body['peerInfo']
+        const endpointInfo = ctx.request.body['endpointInfo']
+        await this.app.addPeer(peerInfo, endpointInfo)
+        ctx.body = ''
+        ctx.response.status = 204
       }
-    } else {
-      res.statusCode = 204
-      res.end()
-    }
-  }
+    })
+    router.route({
+      method: 'get',
+      path: '/peer',
+      handler: async (ctx: Context) => ctx.body = this.app.connector.getPeerList()
+    })
+    router.route({
+      method: 'get',
+      path: '/routes',
+      handler: async (ctx: Context) => ctx.body = this.app.connector.routingTable.getRoutingTable()['items']
+    })
+    koa.use(router.middleware())
 
-  private async getStats () {
-    return this.app.stats.getStatus()
-  }
-
-  private async getAlerts () {
-    return {
-      alerts: this.app.alerts.getAlerts()
-    }
-  }
-
-  private async getBalances () {
-    return this.app.getBalances()
-  }
-
-  private async addPeer (url: string, _data: object) {
-    const peerInfo = _data['peerInfo']
-    const endpointInfo = _data['endpointInfo']
-
-    // TODO use ajv to validate _data
-    if (!peerInfo || !endpointInfo) throw new Error('invalid arguments. need peerInfo and endpointInfo')
-    logger.verbose('adding peer', { peerInfo })
-    await this.app.addPeer(peerInfo, endpointInfo)
-  }
-
-  private async getPeer () {
-    return this.app.connector.getPeerList()
-  }
-
-  private async getRoutes () {
-    return this.app.connector.routingTable.getRoutingTable()['items']
-  }
-
-  private async getHealth () {
-    return 'Status: ok'
+    return koa
   }
 }
