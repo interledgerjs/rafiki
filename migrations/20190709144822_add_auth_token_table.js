e50038e7ae9004b2057066c7b7bde78d8c7874c2
Matt de Haast <matt.dehaast@coil.com>
2019-07-10 11:24:21 +0200

    setup docker container with persistence option and entrypoint

--- migrations/20190709144822_add_auth_token_table.js
+++ migrations/20190709144822_add_auth_token_table.js
@@ -1,8 +1,17 @@
+const nanoid = require('nanoid')
 
 exports.up = function(knex) {
   return knex.schema.createTable('authTokens', function(table) {
     table.string('id').primary()
     table.string('peerId').notNullable()
+  }).then(() => {
+    const adminAuthToken = nanoid(36)
+    console.log('****************************')
+    console.log('Save this token somewhere safe as it is required to perform admin api functions')
+    console.log('****************************')
+    console.log('ADMIN TOKEN: ', adminAuthToken)
+    console.log('****************************')
+    return knex('authTokens').insert({id: adminAuthToken, peerId: 'self'})
   });
 };
 
