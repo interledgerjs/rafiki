bee041a91cee3cc526392ca102bcb540faf99300
DonChangfoot <donovanchangfoot@gmail.com>
2019-08-13 15:58:16 +0200

    feat: modify prepare amount and expiry * utility functions to modify serialized prepare packet

---
+++ test/lib/utils.test.ts
@@ -0,0 +1,41 @@
+import { IlpPrepareFactory } from "../factories/ilpPacket";
+import { modifySerializedIlpPrepareAmount, modifySerializedIlpPrepareExpiry } from "../../src/lib/utils"
+import { serializeIlpPrepare, deserializeIlpPrepare } from "ilp-packet"
+
+describe('modifySerializedIlpPrepareAmount', () => {
+  it('can modify the amount for a length indicator that is not longer than 1 byte', async () => {
+    const prepare = IlpPrepareFactory.build({ amount: '10' })
+  
+    const modifiedPrepare = modifySerializedIlpPrepareAmount(serializeIlpPrepare(prepare), 5n)
+  
+    expect(deserializeIlpPrepare(modifiedPrepare).amount).toBe('5')
+  })
+
+  it('can modify the amount for a length indicator that is longer than 1 byte', async () => {
+    const prepare = IlpPrepareFactory.build({ amount: '10', data: Buffer.alloc(256) })
+  
+    const modifiedPrepare = modifySerializedIlpPrepareAmount(serializeIlpPrepare(prepare), 5n)
+  
+    expect(deserializeIlpPrepare(modifiedPrepare).amount).toBe('5')
+  })
+})
+
+describe('modifySerializedIlpPrepareExpiry', () => {
+  const START_DATE = 1434412800000 // June 16, 2015 00:00:00 GMT
+
+  it('can modify the amount for a length indicator that is not longer than 1 byte', async () => {
+    const prepare = IlpPrepareFactory.build({ amount: '10', expiresAt: new Date(START_DATE) })
+  
+    const modifiedPrepare = modifySerializedIlpPrepareExpiry(serializeIlpPrepare(prepare), new Date(START_DATE + 10 * 1000))
+  
+    expect(deserializeIlpPrepare(modifiedPrepare).expiresAt).toEqual(new Date(START_DATE + 10 * 1000))
+  })
+
+  it('can modify the amount for a length indicator that is longer than 1 byte', async () => {
+    const prepare = IlpPrepareFactory.build({ amount: '10', data: Buffer.alloc(256), expiresAt: new Date(START_DATE) })
+  
+    const modifiedPrepare = modifySerializedIlpPrepareExpiry(serializeIlpPrepare(prepare), new Date(START_DATE + 10 * 1000))
+  
+    expect(deserializeIlpPrepare(modifiedPrepare).expiresAt).toEqual(new Date(START_DATE + 10 * 1000))
+  })
+})
