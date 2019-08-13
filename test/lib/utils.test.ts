2e94b0ba2c02d8737c4bae9de073f300b44d4c5e
DonChangfoot <donovanchangfoot@gmail.com>
2019-08-13 16:01:07 +0200

    refactor: update test names

--- test/lib/utils.test.ts
+++ test/lib/utils.test.ts
@@ -23,7 +23,7 @@ describe('modifySerializedIlpPrepareAmount', () => {
 describe('modifySerializedIlpPrepareExpiry', () => {
   const START_DATE = 1434412800000 // June 16, 2015 00:00:00 GMT
 
-  it('can modify the amount for a length indicator that is not longer than 1 byte', async () => {
+  it('can modify the expiry for a length indicator that is not longer than 1 byte', async () => {
     const prepare = IlpPrepareFactory.build({ amount: '10', expiresAt: new Date(START_DATE) })
   
     const modifiedPrepare = modifySerializedIlpPrepareExpiry(serializeIlpPrepare(prepare), new Date(START_DATE + 10 * 1000))
@@ -31,7 +31,7 @@ describe('modifySerializedIlpPrepareExpiry', () => {
     expect(deserializeIlpPrepare(modifiedPrepare).expiresAt).toEqual(new Date(START_DATE + 10 * 1000))
   })
 
-  it('can modify the amount for a length indicator that is longer than 1 byte', async () => {
+  it('can modify the expiry for a length indicator that is longer than 1 byte', async () => {
     const prepare = IlpPrepareFactory.build({ amount: '10', data: Buffer.alloc(256), expiresAt: new Date(START_DATE) })
   
     const modifiedPrepare = modifySerializedIlpPrepareExpiry(serializeIlpPrepare(prepare), new Date(START_DATE + 10 * 1000))
